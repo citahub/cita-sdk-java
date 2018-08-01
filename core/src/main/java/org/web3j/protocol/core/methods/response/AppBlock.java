c1e254321ab7f97159d195810dcffd3e75f70b68
Qingyangkong <frankkongnj@yahoo.com>
2018-08-01 15:21:13 +0800

    fix the issue block strcuture mismatch

--- core/src/main/java/org/web3j/protocol/core/methods/response/AppBlock.java
+++ core/src/main/java/org/web3j/protocol/core/methods/response/AppBlock.java
@@ -8,15 +8,13 @@ import java.util.List;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import org.web3j.protocol.ObjectMapperFactory;
 import org.web3j.protocol.core.Response;
 import org.web3j.utils.Numeric;
+
 
 /**
  * Block object returned by:
@@ -50,19 +48,48 @@ public class AppBlock extends Response<AppBlock.Block> {
         return getResult() == null;
     }
 
+    public static class TendermintCommit {
+        private String commitAddress;
+        private String commit;
 
-    public static class Proof {
+        public TendermintCommit() {};
+
+        public TendermintCommit(String commitAddress, String commit) {
+            this.commitAddress = commitAddress;
+            this.commit = commit;
+        }
+
+        public String getCommitAddress() {
+            return this.commitAddress;
+        }
+
+        public void setCommitAddress(String commitAddress) {
+            this.commitAddress = commitAddress;
+        }
+
+        public String getCommit() {
+            return this.commit;
+        }
+
+        public void setCommit(String commit) {
+            this.commit = commit;
+        }
+    }
+
+    public static class Tendermint {
         private String proposal;
         private String height;
         private String round;
+        private TendermintCommit[] tendermintCommits;
 
-        public Proof() {
-        }
+        Tendermint() {};
 
-        public Proof(String proposal, String height, String round) {
+        Tendermint(String proposal, String height,
+                   String round, TendermintCommit[] tendermintCommits) {
             this.proposal = proposal;
             this.height = height;
             this.round = round;
+            this.tendermintCommits = tendermintCommits;
         }
 
         public String getProposal() {
@@ -89,27 +116,36 @@ public class AppBlock extends Response<AppBlock.Block> {
             this.round = round;
         }
 
+        public TendermintCommit[] getTendermintCommits() {
+            return this.tendermintCommits;
+        }
+
+        public void setTendermintCommits(
+                TendermintCommit[] tendermintCommits) {
+            this.tendermintCommits = tendermintCommits;
+        }
+
         @Override
         public boolean equals(Object o) {
             if (this == o) {
                 return true;
             }
-            if (!(o instanceof Proof)) {
+            if (!(o instanceof Tendermint)) {
                 return false;
             }
 
-            Proof proof = (Proof) o;
+            Tendermint tendermint = (Tendermint) o;
 
             if (getHeight() != null
-                    ? !getHeight().equals(proof.getHeight()) : proof.getHeight() != null) {
+                    ? !getHeight().equals(tendermint.getHeight()) : tendermint.getHeight() != null) {
                 return false;
             }
             if (getProposal() != null
-                    ? !getProposal().equals(proof.getProposal()) : proof.getProposal() != null) {
+                    ? !getProposal().equals(tendermint.getProposal()) : tendermint.getProposal() != null) {
                 return false;
             }
             return (getRound() != null
-                    ? getRound().equals(proof.getRound()) : proof.getRound() == null);
+                    ? getRound().equals(tendermint.getRound()) : tendermint.getRound() == null);
         }
 
         @Override
@@ -118,6 +154,26 @@ public class AppBlock extends Response<AppBlock.Block> {
             result = 31 * result + (getHeight() != null ? getHeight().hashCode() : 0);
             result = 31 * result + (getRound() != null ? getRound().hashCode() : 0);
             return result;
+        }
+    }
+
+
+    public static class Proof {
+        private Tendermint Tendermint;
+
+        public Proof() {
+        }
+
+        public Proof(Tendermint tendermint) {
+            this.Tendermint = tendermint;
+        }
+
+        public Tendermint getTendermint() {
+            return Tendermint;
+        }
+
+        public void setTendermint(Tendermint tendermint) {
+            this.Tendermint = tendermint;
         }
     }
 
@@ -295,21 +351,20 @@ public class AppBlock extends Response<AppBlock.Block> {
     }
 
     public static class Body {
-        private List<TransactionResult> transactions;
+        private List<TransactionObject> transactions;
 
         public Body() {
         }
 
-        public Body(List<TransactionResult> transactions) {
+        public Body(List<TransactionObject> transactions) {
             this.transactions = transactions;
         }
 
-        public List<TransactionResult> getTransactions() {
+        public List<TransactionObject> getTransactions() {
             return transactions;
         }
 
-        @JsonDeserialize(using = AppBlock.ResultTransactionDeserialiser.class)
-        public void setTransactions(List<TransactionResult> transactions) {
+        public void setTransactions(List<TransactionObject> transactions) {
             this.transactions = transactions;
         }
 
@@ -423,45 +478,45 @@ public class AppBlock extends Response<AppBlock.Block> {
     public interface TransactionResult<T> {
         T get();
     }
-
-    public static class TransactionHash implements TransactionResult<String> {
-        private String value;
-
-        public TransactionHash() {
-        }
-
-        public TransactionHash(String value) {
-            this.value = value;
-        }
-
-        @Override
-        public String get() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof TransactionHash)) {
-                return false;
-            }
-
-            TransactionHash that = (TransactionHash) o;
-
-            return value != null ? value.equals(that.value) : that.value == null;
-        }
-
-        @Override
-        public int hashCode() {
-            return value != null ? value.hashCode() : 0;
-        }
-    }
+//
+//    public static class TransactionHash implements TransactionResult<String> {
+//        private String value;
+//
+//        public TransactionHash() {
+//        }
+//
+//        public TransactionHash(String value) {
+//            this.value = value;
+//        }
+//
+//        @Override
+//        public String get() {
+//            return value;
+//        }
+//
+//        public void setValue(String value) {
+//            this.value = value;
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) {
+//                return true;
+//            }
+//            if (!(o instanceof TransactionHash)) {
+//                return false;
+//            }
+//
+//            TransactionHash that = (TransactionHash) o;
+//
+//            return value != null ? value.equals(that.value) : that.value == null;
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return value != null ? value.hashCode() : 0;
+//        }
+//    }
 
     public static class TransactionObject extends Transaction
             implements TransactionResult<Transaction> {
@@ -483,49 +538,67 @@ public class AppBlock extends Response<AppBlock.Block> {
         }
     }
 
-    public static class ResultTransactionDeserialiser
-            extends JsonDeserializer<List<TransactionResult>> {
-
-        private ObjectReader objectReader = ObjectMapperFactory.getObjectReader();
-
-        @Override
-        public List<TransactionResult> deserialize(
-                JsonParser jsonParser,
-                DeserializationContext deserializationContext) throws IOException {
-
-            List<TransactionResult> transactionResults = new ArrayList<>();
-            JsonToken nextToken = jsonParser.nextToken();
-
-            if (nextToken == JsonToken.START_OBJECT) {
-                Iterator<TransactionObject> transactionObjectIterator =
-                        objectReader.readValues(jsonParser, TransactionObject.class);
-                while (transactionObjectIterator.hasNext()) {
-                    transactionResults.add(transactionObjectIterator.next());
-                }
-            } else if (nextToken == JsonToken.VALUE_STRING) {
-                jsonParser.getValueAsString();
-
-                Iterator<TransactionHash> transactionHashIterator =
-                        objectReader.readValues(jsonParser, TransactionHash.class);
-                while (transactionHashIterator.hasNext()) {
-                    transactionResults.add(transactionHashIterator.next());
-                }
-            }
-
-            return transactionResults;
-        }
-    }
-
     public static class ResponseDeserialiser extends JsonDeserializer<Block> {
-
-        private ObjectReader objectReader = ObjectMapperFactory.getObjectReader();
 
         @Override
         public Block deserialize(
                 JsonParser jsonParser,
                 DeserializationContext deserializationContext) throws IOException {
             if (jsonParser.getCurrentToken() != JsonToken.VALUE_NULL) {
-                return objectReader.readValue(jsonParser, Block.class);
+                JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+                //block meta
+                String blockVersion = node.get("version").asText();
+                String blockHash = node.get("hash").asText();
+
+                //block header
+                JsonNode headerNode = node.get("header");
+                Long timeStamp = headerNode.get("timestamp").asLong();
+                String prevHash = headerNode.get("prevHash").asText();
+                String number = headerNode.get("number").asText();
+                String stateRoot = headerNode.get("stateRoot").asText();
+                String transactionsRoot = headerNode.get("transactionsRoot").asText();
+                String receiptsRoot = headerNode.get("receiptsRoot").asText();
+                String gasUsed = headerNode.get("gasUsed").asText();
+
+                //proof tendermint
+                JsonNode proofNode = node.get("header").get("proof").get("Tendermint");
+                String proposal = proofNode.get("proposal").asText();
+                String height = proofNode.get("height").asText();
+                String round = proofNode.get("round").asText();
+
+                //proof tendermint commits
+                List<TendermintCommit> tendermintCommits = new ArrayList<>();
+                JsonNode commitsNode = node.get("header").get("proof").get("Tendermint").get("commits");
+                Iterator<String> commitsAddress = commitsNode.fieldNames();
+                while(commitsAddress.hasNext()) {
+                    String commitAddress = commitsAddress.next();
+                    String commit = commitsNode.get(commitAddress).asText();
+                    tendermintCommits.add(new TendermintCommit(commitAddress, commit));
+                }
+
+                //body transactions
+                List<TransactionObject> transactionObjs = new ArrayList<TransactionObject>();
+                JsonNode transactionNode = node.get("body").get("transactions");
+                Iterator<JsonNode> txNodes = transactionNode.elements();
+                while(txNodes.hasNext()) {
+                    JsonNode txNode = txNodes.next();
+                    TransactionObject txToAdd = new TransactionObject();
+                    txToAdd.setHash(txNode.get("hash").asText());
+                    txToAdd.setContent(txNode.get("content").asText());
+                    txToAdd.setBlockHash(blockHash);
+                    txToAdd.setBlockNumber(number);
+                    transactionObjs.add(txToAdd);
+                }
+
+                Tendermint tendermint = new Tendermint(
+                        proposal, height, round,
+                        tendermintCommits.toArray(
+                                new TendermintCommit[tendermintCommits.size()]));
+
+                Header header = new Header(timeStamp, prevHash, number, stateRoot,
+                        transactionsRoot, receiptsRoot, gasUsed, new Proof(tendermint));
+                Body body = new Body(transactionObjs);
+                return new Block(blockVersion, blockHash, header, body);
             } else {
                 return null;  // null is wrapped by Optional in above getter
             }
