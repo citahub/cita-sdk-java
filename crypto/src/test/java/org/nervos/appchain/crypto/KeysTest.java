31cb6272bad796db810521c614778cfb7a0c79d7
Qingyangkong <frankkongnj@yahoo.com>
2018-09-25 17:10:58 +0800

    add methods to verify addr and priv key

--- crypto/src/test/java/org/nervos/appchain/crypto/KeysTest.java
+++ crypto/src/test/java/org/nervos/appchain/crypto/KeysTest.java
@@ -13,8 +13,10 @@ import org.nervos.appchain.utils.Strings;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class KeysTest {
 
@@ -102,5 +104,31 @@ public class KeysTest {
     @Test(expected = RuntimeException.class)
     public void testDeserializeInvalidKey() {
         Keys.deserialize(new byte[0]);
+    }
+
+    @Test
+    public void testVerifyAddress() {
+        assertTrue(Keys.verifyAddress("0xbac68e5cb986ead0253e0632da1131a0a96efa18"));
+        assertTrue(Keys.verifyAddress("0Xbac68e5cb986ead0253e0632da1131a0a96efa18"));
+        assertTrue(Keys.verifyAddress("bac68e5cb986ead0253e0632da1131a0a96efa18"));
+        assertFalse(Keys.verifyPrivateKey("xbac68e5cb986ead0253e0632da1131a0a96efa18"));
+        assertFalse(Keys.verifyPrivateKey("0bac68e5cb986ead0253e0632da1131a0a96efa18"));
+        assertFalse(Keys.verifyPrivateKey("gac68e5cb986ead0253e0632da1131a0a96efa18"));
+    }
+
+    @Test
+    public void testVerifyPrivateKey() {
+        assertTrue(Keys.verifyPrivateKey(
+                "0x02b8f18d92354b055bd02cd51449b05b7b97104931f10485d1df905e4e70fbca"));
+        assertTrue(Keys.verifyPrivateKey(
+                "0X02b8f18d92354b055bd02cd51449b05b7b97104931f10485d1df905e4e70fbca"));
+        assertTrue(Keys.verifyPrivateKey(
+                "02b8f18d92354b055bd02cd51449b05b7b97104931f10485d1df905e4e70fbca"));
+        assertFalse(Keys.verifyPrivateKey(
+                "x02b8f18d92354b055bd02cd51449b05b7b97104931f10485d1df905e4e70fbca"));
+        assertFalse(Keys.verifyPrivateKey(
+                "002b8f18d92354b055bd02cd51449b05b7b97104931f10485d1df905e4e70fbca"));
+        assertFalse(Keys.verifyPrivateKey(
+                "h2b8f18d92354b055bd02cd51449b05b7b97104931f10485d1df905e4e70fbca"));
     }
 }
