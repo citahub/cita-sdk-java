6476ef064c99ac1cd628e093fa317ad52bf4d610
Qingyangkong <frankkongnj@yahoo.com>
2018-10-10 21:59:23 +0800

    add system contract for node manager

---
+++ core/src/main/java/org/nervos/appchain/protocol/system/AppChainSystemContract.java
@@ -0,0 +1,115 @@
+package org.nervos.appchain.protocol.system;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.nervos.appchain.abi.FunctionEncoder;
+import org.nervos.appchain.abi.TypeReference;
+import org.nervos.appchain.abi.datatypes.Function;
+import org.nervos.appchain.abi.datatypes.Type;
+import org.nervos.appchain.protocol.Nervosj;
+import org.nervos.appchain.protocol.core.DefaultBlockParameter;
+import org.nervos.appchain.protocol.core.methods.request.Call;
+import org.nervos.appchain.protocol.core.methods.response.AppCall;
+/**
+ * Please check system smart contracts at
+ * https://github.com/cryptape/cita/tree/develop/scripts/contracts/src
+ */
+public interface AppChainSystemContract {
+    DefaultBlockParameter DEFAULT_BLOCK_PARAMETER
+            = DefaultBlockParameter.valueOf("latest");
+    //Node manager
+    String NODE_MANAGER_ADDR = "0xffffffffffffffffffffffffffffffffff020001";
+    String NODE_MANAGER_LIST_NODE = "listNode";
+    String NODE_MANAGER_GET_STATUS = "getStatus";
+    String NODE_MANAGER_LIST_STAKE = "listStake";
+    String NODE_MANAGER_STAKE_PERMILLAGE = "stakePermillage";
+    String NODE_MANAGER_APPROVE_NODE = "approveNode";
+    String NODE_MANAGER_DELETE_NODE = "deleteNode";
+    String NODE_MANAGER_SET_STAKE = "setStake";
+    //Quota manager
+    String QUOTA_MANAGER_ADDR = "0xffffffffffffffffffffffffffffffffff020003";
+    String QUOTA_MANAGER_SET_BQL = "setBQL";
+    String QUOTA_MANAGER_SET_DEFAULT_AQL = "setDefaultAQL";
+    String QUOTA_MANAGER_SET_AQL = "setAQL";
+    String QUOTA_MANAGER_GET_BQL = "getBQL";
+    String QUOTA_MANAGER_GET_DEFAULT_AQL = "getDefaultAQL";
+    String QUOTA_MANAGER_GET_AQL = "getAQL";
+    String QUOTA_MANAGER_GET_ACCOUNTS = "getAccounts";
+    String QUOTA_MANAGER_GET_QUOTAS = "getQuotas";
+    //Price Manager
+    String PRICE_MANAGER_ADDR = "0xffffffffffffffffffffffffffffffffff020010";
+    String PRICE_MANAGER_GET_QUOTA_PRICE = "getQuotaPrice";
+    //Permission manager
+    String PERMISSION_MANAGER_ADDR = "0xffffffffffffffffffffffffffffffffff020010";
+    //Permission manager manipulation
+    String PERMISSION_MANAGER_NEW_PERMISSION = "newPermission";
+    String PERMISSION_MANAGER_DELETE_PERMISSION = "deletePermission";
+    String PERMISSION_MANAGER_UPDATE_PERMISSION_NAME = "updatePermissionName";
+    String PERMISSION_MANAGER_ADD_RESOURCES = "addResources";
+    String PERMISSION_MANAGER_DELETE_RESOURCES = "deleteResources";
+    String PERMISSION_MANAGER_SET_AUTHORIZATION = "setAuthorization";
+    String PERMISSION_MANAGER_SET_AUTHORIZATIONS = "setAuthorizations";
+    String PERMISSION_MANAGER_CANCEL_AUTHORIZATION = "cancelAuthorization";
+    String PERMISSION_MANAGER_CANCEL_AUTHORIZATIONS = "cancelAuthorizations";
+    String PERMISSION_MANAGER_CLEAR_AUTHORIZATION = "clearAuthorization";
+    //Permission manager query
+    String PERMISSION_MANAGER_QUERY_ALL_ACCOUNTS = "queryAllAccounts";
+    String PERMISSION_MANAGER_QUERY_PERMISSION = "queryPermissions";
+    String PERMISSION_MANAGER_QUERY_ACCOUNTS = "queryAccounts";
+    String PERMISSION_MANAGER_CHECK_PERMISSON = "checkPermission";
+    String PERMISSION_MANAGER_CHECK_RESOURCE = "checkResource";
+    String PERMISSION_MANAGER_IN_PERMISSION = "inPermission";
+    String PERMISSION_MANAGER_QUERY_INFO = "queryInfo";
+    String PERMISSION_MANAGER_QUERY_NAME = "queryName";
+    String PERMISSION_MANAGER_QUERY_RESOURCE = "queryResource";
+    //User manager
+    String USER_MANAGER_ADDR = "0xffffffffffffffffffffffffffffffffff020010";
+    //User manager manipulation
+    String USER_MANAGER_NEW_GROUP = "newGroup";
+    String USER_MANAGER_DELETE_GROUP = "deleteGroup";
+    String USER_MANAGER_UPDATE_GROUP_NAME = "updateGroupName";
+    String USER_MANAGER_ADD_ACCOUNTS = "addAccounts";
+    String USER_MANAGER_DELETE_ACCOUNTS = "deleteAccounts";
+    //User manager query (within group)
+    String USER_MANAGER_QUERY_INFO = "queryInfo";
+    String USER_MANAGER_QUERY_NAME = "queryName";
+    String USER_MANAGER_QUERY_ACCOUNTS = "queryAccounts";
+    String USER_MANAGER_QUERY_CHILD = "queryChild";
+    String USER_MANAGER_QUERY_CHILD_LENGHT = "queryChildLength";
+    String USER_MANAGER_QUERY_PARENT = "queryParent";
+    String USER_MANAGER_IN_GROUP = "inGroup";
+    //User manager query (query group)
+    String USER_MANAGER_CHECK_SCOPE = "checkScope";
+    String USER_MANAGER_QUERY_GROUPS = "queryGroups";
+    static List<TypeReference<Type>> convert(List<TypeReference<?>> input) {
+        List<TypeReference<Type>> result = new ArrayList<>(input.size());
+        result.addAll(input.stream()
+                .map(typeReference -> (TypeReference<Type>) typeReference)
+                .collect(Collectors.toList()));
+        return result;
+    }
+    static String encodeCall(String methodName) {
+        Function callFunc = new Function(
+                methodName,
+                Collections.emptyList(),
+                Collections.emptyList());
+        return FunctionEncoder.encode(callFunc);
+    }
+    static String encodeFunction(String methodName, List<Type> inputParameters) {
+        Function func = new Function(
+                methodName,
+                inputParameters,
+                Collections.emptyList()
+        );
+        return FunctionEncoder.encode(func);
+    }
+    static AppCall sendCall(String from, String addr,
+                            String callData, Nervosj service)
+            throws IOException {
+        Call call = new Call(from, addr, callData);
+        return service.appCall(call, DEFAULT_BLOCK_PARAMETER).send();
+    }
+    int getQuotaPrice(String from) throws IOException;
+}
---
+++ core/src/main/java/org/nervos/appchain/protocol/system/Util.java
@@ -0,0 +1,56 @@
+package org.nervos.appchain.protocol.system;
+import java.math.BigInteger;
+import java.util.Random;
+import org.nervos.appchain.protocol.Nervosj;
+import org.nervos.appchain.utils.Numeric;
+/**
+ * remove this class later.
+ * **/
+public class Util {
+    static String getNonce() {
+        Random random = new Random(System.currentTimeMillis());
+        return String.valueOf(Math.abs(random.nextLong()));
+    }
+    static BigInteger getCurrentHeight(Nervosj service) {
+        return getCurrentHeight(service, 3);
+    }
+    private static BigInteger getCurrentHeight(Nervosj service, int retry) {
+        int count = 0;
+        long height = -1;
+        while (count < retry) {
+            try {
+                height = service.appBlockNumber().send().getBlockNumber().longValue();
+            } catch (Exception e) {
+                height = -1;
+                System.out.println("getBlockNumber failed retry ..");
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e1) {
+                    System.out.println("failed to get block number, Exception: " + e1);
+                    System.exit(1);
+                }
+            }
+            count++;
+        }
+        if (height == -1) {
+            System.out.println("Failed to get block number after " + count + " times.");
+            System.exit(1);
+        }
+        return BigInteger.valueOf(height);
+    }
+    static BigInteger getValidUtilBlock(Nervosj service, int validUntilBlock) {
+        return getCurrentHeight(service).add(
+                BigInteger.valueOf(validUntilBlock));
+    }
+    static BigInteger getValidUtilBlock(Nervosj service) {
+        return getCurrentHeight(service).add(BigInteger.valueOf(88));
+    }
+    static String addUpTo64Hex(String hexStr) {
+        String result = Numeric.cleanHexPrefix(hexStr);
+        int len = 64 - result.length();
+        for (int i = 0; i < len; i++) {
+            result = "0" + result;
+        }
+        return Numeric.prependHexPrefix(result);
+    }
+}
