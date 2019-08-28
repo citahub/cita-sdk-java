9b653c0a750499f75514b2db1525d295e56eb1af
qiaorong <qiaorong@nuoye.link>
2019-08-28 09:53:23 +0800

    Add some new functions for group management, modify queryGroups and newGroup for more rubust.

--- tests/src/main/java/com/cryptape/cita/tests/GroupManagerExample.java
+++ tests/src/main/java/com/cryptape/cita/tests/GroupManagerExample.java
@@ -3,10 +3,14 @@ package com.cryptape.cita.tests;
 
 import com.cryptape.cita.protocol.core.methods.response.Log;
 import com.cryptape.cita.protocol.system.CITASystemContract;
+import com.cryptape.cita.protocol.system.CITAjSystemContract;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Group and Account Management
@@ -14,21 +18,68 @@ import java.util.List;
  * https://docs.citahub.com/zh-CN/cita/account-permission/account
  */
 public class GroupManagerExample extends SystemContractExample {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        // query groups
-        List<String> groupAddress = sysContract.queryGroups("0xfFFfFFFFFffFFfffFFFFfffffFffffFFfF020009");
-        System.out.println("groupAddress: " + groupAddress);
+    static String senderAddr;
+    static String adminPrivateKey;
+    static int version;
+    static BigInteger chainId;
 
-        // create a group
-        String transcationHash = sysContract.newGroup("0xfFFfFFFFFffFFfffFFFFfffffFffffFFfF020009",
-                "newGroup",
-                Arrays.asList("e1c4021742730ded647590a1686d5c4bfcbae0b0", "45a50f45cb81c8aedeab917ea0cd3c9178ebdcae"),
-                adminPriavteKey,
-                version,
-                chainId);
-        System.out.println("transcationHash: " + transcationHash);
-        // get receipt by hash
-        Log log = CITASystemContract.getReceiptLog(service, transcationHash, 0);
-        System.out.println("groupAddress: " + (log == null ? "null" : log.getAddress()));
+    static {
+        Config conf = new Config();
+        conf.buildService(false);
+        service = conf.service;
+        senderAddr = conf.primaryAddr;
+        adminPrivateKey = conf.adminPrivateKey;
+        version = TestUtil.getVersion(service);
+        chainId = TestUtil.getChainId(service);
+    }
+    public static void main(String[] args) throws Exception {
+        CITAjSystemContract sysContract = new CITAjSystemContract(service);
+
+        // New group
+        List<String> addresses = new ArrayList<>();
+        addresses.add("0x1c6eebf136ee234caff3a95e0d9d22e40c9ac4ca");
+        String newGroupAddr = sysContract.newGroup("vlk1", addresses, adminPrivateKey, version, chainId);
+        if ("".equals(newGroupAddr)) {
+            System.out.println("New group failed！");
+            return;
+        }
+        System.out.println("Address for new group: " + newGroupAddr);
+        TimeUnit.SECONDS.sleep(10);
+
+        // Update group name
+        boolean updated = sysContract.updateGroupName(newGroupAddr, "TokenNew", adminPrivateKey, version, chainId);
+        System.out.println("Group updated: " + updated);
+        TimeUnit.SECONDS.sleep(10);
+
+        // Add accounts
+        List<String> addressNeedBeAdded = new ArrayList<>();
+        addressNeedBeAdded.add("0xbac68e5cb986ead0253e0632da1131a0a96efa18");
+        updated = sysContract.addAccounts(newGroupAddr, addressNeedBeAdded, adminPrivateKey, version, chainId);
+        System.out.println("Account is added: " + updated);
+        TimeUnit.SECONDS.sleep(10);
+
+        // Delete accounts
+        updated = sysContract.deleteAccounts(newGroupAddr, addressNeedBeAdded, adminPrivateKey, version, chainId);
+        System.out.println("Account is deleted: " + updated);
+        TimeUnit.SECONDS.sleep(10);
+
+        // Check scope
+        updated = sysContract.checkScope(newGroupAddr, adminPrivateKey, version, chainId);
+        System.out.println("Check scope: " + updated);
+        TimeUnit.SECONDS.sleep(10);
+
+        // Query groups
+        List<String> res = sysContract.queryGroups(senderAddr);
+        if (res != null) {
+            for (String s : res) {
+                System.out.println("Group address: " + s);
+            }
+        }
+        TimeUnit.SECONDS.sleep(10);
+
+        // Delete group
+        updated = sysContract.deleteGroup(newGroupAddr, adminPrivateKey, version, chainId);
+        System.out.println("Group deleted: " + updated);
+        TimeUnit.SECONDS.sleep(10);
     }
 }
